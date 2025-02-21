cfa1821dcbda6448c29f67e733e674931b09e50f
edukowicz <26.ellie.dukowicz@carolinaday.org>
2025-02-21 14:02:53 +0000

    message

--- Main.java
+++ Main.java
@@ -6,15 +6,29 @@ public class Main
      //Instantiating and testing a Player object
      System.out.println("Testing Player Object - Warrior argument");
      Player player1 = new Player("Warrior");
+     System.out.println(player1);
+     System.out.println(player1.getRole());
+     System.out.println(player1.getH());
+     System.out.println();
      
      
      //Instantiating and testing a Warrior Object  
      System.out.println("Testing Warrior Object");
      Warrior player2 = new Warrior();
+     System.out.println(player2);
+     System.out.println(player2.getRole());
+     System.out.println(player2.getH());
+     System.out.println();
+
      
     //Instantiating and Testing a Wizard Object
      System.out.println("Testing Wizard Object");
      Wizard player3 = new Wizard();
+     System.out.println(player3);
+     System.out.println(player3.getRole());
+     System.out.println(player3.getH());
+     System.out.println();
+
      
     //Testing Modified Warrior
      System.out.println("Testing Updated Warrior Object");
